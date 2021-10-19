f1efea9dd721a3642b7d917da8fb49449560db0c
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2021-10-19 09:56:57 +0900

    :sparkles: App.tsx に Row コンポーネントを追加し API からデータを取得してみる

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Row } from './components/Row';
 import './App.css';
+import { requests } from './request';
 
 function App() {
   return (
     <div className="App">
+        <Row
+          title="NETFLIX ORIGUINALS"
+          fetchUrl={requests.feachNetflixOriginals}
+          isLargeRow
+        />
+        <Row title="Top Rated" fetchUrl={requests.feactTopRated} />
+        <Row title="Action Movies" fetchUrl={requests.feactActionMovies} />
+        <Row title="Comedy Movies" fetchUrl={requests.feactComedyMovies} />
+        <Row title="Horror Movies" fetchUrl={requests.feactHorrorMovies} />
+        <Row title="Romance Movies" fetchUrl={requests.feactRomanceMovies} />
+        <Row title="DOcumentaries" fetchUrl={requests.feactDocumentMovies} />
     </div>
   );
 }
