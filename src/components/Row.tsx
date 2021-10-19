f92616323c4797cde5776084c085bff289ea34de
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2021-10-19 10:11:59 +0900

    :sparkles: APIから取得した映画の画像が表示されるようにスタイリング

--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -1,5 +1,8 @@
 import { api as axios } from '../request';
 import React, { useEffect, useState } from 'react';
+import './Row.scss'
+
+const base_url = "https://image.tmdb.org/t/p/original";
 
 type Props = {
   title: string;
@@ -16,7 +19,7 @@ type Movie = {
   backdrop_path: string;
 };
 
-export const Row = ({ title, fetchUrl }: Props) => {
+export const Row = ({ title, fetchUrl, isLargeRow }: Props) => {
   const [movies, setMovies] = useState<Movie[]>([]);
 
   useEffect(() => {
@@ -30,7 +33,22 @@ export const Row = ({ title, fetchUrl }: Props) => {
 
   console.log(movies);
 
-  return(
-      <div className="Row" /> 
-  );
+  return (
+      <div className="Row">
+        <h2>{title}</h2>
+        <div className="Row-posters">
+          {/* ポスターコンテンツ */}
+          {movies.map((movie, i) => (
+            <img
+              key={movie.id}
+              className={`Row-poster ${isLargeRow && "Row-poster-large"}`}
+              src={`${base_url}${
+                isLargeRow ? movie.poster_path : movie.backdrop_path
+              }`}
+              alt={movie.name}
+            />
+          ))}
+        </div>
+      </div>
+    );
 };
