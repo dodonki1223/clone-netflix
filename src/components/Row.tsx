8522c22d695902620c9fee67848c04eee0964802
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2021-11-30 14:37:32 +0900

    :sparkles: 画像クリックで YouTube の動画が表示される機能を追加

--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -1,4 +1,8 @@
-import { useMovies } from '../hooks/useMovies';
+import { useMovies, Movie } from '../hooks/useMovies';
+import { api as axios, API_KEY } from '../request';
+import { AxiosResponse } from 'axios';
+import { useState } from 'react';
+import YouTube from 'react-youtube';
 import './Row.scss'
 
 const base_url = "https://image.tmdb.org/t/p/original";
@@ -9,10 +13,39 @@ type Props = {
   isLargeRow?: boolean;
 };
 
+//trailerのoption
+type Options = {
+  height: string;
+  width: string;
+  playerVars: {
+    autoplay: 0 | 1 | undefined;
+  };
+};
+
+
 export const Row = ({ title, fetchUrl, isLargeRow }: Props) => {
   const { movies } = useMovies(fetchUrl);
+  const [trailerUrl, setTrailerUrl] = useState<string | null>("");
 
-  console.log(movies);
+  const opts: Options = {
+    height: "390",
+    width: "640",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = async (movie: Movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      let trailerUrl: AxiosResponse<{results:{key:string}[]}> = await axios.get(
+        `/movie/${movie.id}/videos?api_key=${API_KEY}`
+      );
+      setTrailerUrl(trailerUrl.data.results[0]?.key);
+    }
+  };
 
   return (
       <div className="Row">
@@ -27,9 +60,11 @@ export const Row = ({ title, fetchUrl, isLargeRow }: Props) => {
                 isLargeRow ? movie.poster_path : movie.backdrop_path
               }`}
               alt={movie.name}
+              onClick={() => handleClick(movie)}
             />
           ))}
         </div>
+        {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
       </div>
     );
 };
